750aeb33842f7343ec818332efd2c78afa0e97cf
Saurabhrana2290 <saurabhrana532@gmail.com>
2021-08-20 19:41:06 +0530

    docs updated app.js

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var divOutput = document.querySelector("#output");
+
+var serverUrl = "https://api.funtranslations.com/translate/minion.json";
+function  getTranslationUrl(input){
+    return serverUrl + "?" + "text=" + input;
+}
+function errorHandler(error){
+    console.log("error",error);
+}
 function clickEventHandler(){
-    divOutput.innerHTML="ajajajjajaj  " + txtInput.value;
+    var inputText = txtInput.value;
+
+    fetch(getTranslationUrl(inputText))
+    .then(
+        response => response.json()
+    )
+    .then(
+        json => {
+            var translatedText=json.contents.translated;
+            divOutput.innerText = translatedText;
+        }
+    )
+    .catch(errorHandler);
 }
 
 btnTranslate.addEventListener("click",clickEventHandler);
